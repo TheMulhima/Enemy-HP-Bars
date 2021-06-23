b66e09f214c26ab4d4482564304d945461cbab37
Mulhima <81007262+TheMulhima@users.noreply.github.com>
2021-06-23 11:39:50 +0400

    Port to 1.5

--- src/Settings.cs
+++ src/Settings.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 
 namespace EnemyHPBar
 {
-	public class Settings : ModSettings
-	{
-		public float fgScale = 1.0f;
-		public float bgScale = 1.0f;
-		public float mgScale = 1.0f;
-		public float olScale = 1.0f;
-		public float bossfgScale = 1.0f;
-		public float bossbgScale = 1.0f;
-		public float bossolScale = 1.0f;
-	}
+    public class Settings
+    {
+        public float fgScale = 1.0f;
+        public float bgScale = 1.0f;
+        public float mgScale = 1.0f;
+        public float olScale = 1.0f;
+        public float bossfgScale = 1.0f;
+        public float bossbgScale = 1.0f;
+        public float bossolScale = 1.0f;
+    }
 }
